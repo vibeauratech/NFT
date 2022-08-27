52313c499487a3bdbd272c867092125dc1c2dc31
vibeauratech <emmanuelakharia@gmail.com>
2022-08-27 08:34:37 +0100

    yes

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 document.getElementById("menu").innerText = 600;
 
-let count = 10 * 10;
-
-
-
-let bonusPoints = 50;
-
-bonusPoints = bonusPoints + 50;
-
-bonusPoints = bonusPoints - 25;
-
-bonusPoints = bonusPoints + 70;
-
+function increment() {
+    console.log("button was clicked")
+}
